c6bb439bd97fcd29914968e38e2453eb9341e1fd
ofirCohen <ofir2161324@gmail.com>
2023-12-27 00:59:33 +0200

    covered basic jwt testing (register,login,me,get all users) with most use cases, need to check how to test the session expired thingy + some error[look in coverage index.html]

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -31,41 +31,72 @@ describe("Auth tests", () => {
     expect(response.statusCode).toBe(201);
   });
 
-  //   test("Test Register exist email", async () => {
-  //     const response = await request(app).post("/auth/register").send(user);
-  //     expect(response.statusCode).toBe(406);
-  //   });
+  test("Test Register exist email", async () => {
+    const response = await request(app).post("/api/auth/register").send(user);
+    console.log("response: " + JSON.stringify(response));
+    expect(response.statusCode).toBe(409);
+  });
 
-  //   test("Test Register missing password", async () => {
-  //     const response = await request(app).post("/auth/register").send({
-  //       email: "test@test.com",
-  //     });
-  //     expect(response.statusCode).toBe(400);
-  //   });
+  test("Test Register missing parameters", async () => {
+    const response = await request(app)
+      .post("/api/auth/register")
+      .send({ email: user.email });
+    console.log("response: " + JSON.stringify(response));
 
-  //   test("Test Login", async () => {
-  //     const response = await request(app).post("/auth/login").send(user);
-  //     expect(response.statusCode).toBe(200);
-  //     accessToken = response.body.accessToken;
-  //     expect(accessToken).toBeDefined();
-  //   });
+    expect(response.statusCode).toBe(400);
+  });
 
-  //   test("Test forbidden access without token", async () => {
-  //     const response = await request(app).get("/student");
-  //     expect(response.statusCode).toBe(401);
-  //   });
+  let accessToken: string;
 
-  //   test("Test access with valid token", async () => {
-  //     const response = await request(app)
-  //       .get("/student")
-  //       .set("Authorization", "JWT " + accessToken);
-  //     expect(response.statusCode).toBe(200);
-  //   });
+  test("Test Login", async () => {
+    const response = await request(app).post("/api/auth/login").send({
+      email: user.email,
+      password: user.password,
+    });
+    expect(response.statusCode).toBe(200);
+    console.log("response: " + JSON.stringify(response));
 
-  //   test("Test access with invalid token", async () => {
-  //     const response = await request(app)
-  //       .get("/student")
-  //       .set("Authorization", "JWT 1" + accessToken);
-  //     expect(response.statusCode).toBe(401);
-  //   });
+    accessToken = response.body.accessToken;
+    expect(accessToken).toBeDefined();
+  });
+
+  test("Test forbidden access without token", async () => {
+    const response = await request(app).get("/api/users/me");
+    console.log("response: " + JSON.stringify(response));
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Test access with valid token", async () => {
+    const response = await request(app)
+      .get("/api/users/me")
+      .set("Authorization", "Bearer " + accessToken);
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Test access with invalid token", async () => {
+    const response = await request(app)
+      .get("/api/users/me")
+      .set("Authorization", "Bearer typo" + accessToken);
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("Test forbidden access with valid token but no role permissions", async () => {
+    const response = await request(app)
+      .get("/api/users")
+      .set("Authorization", "Bearer " + accessToken);
+    console.log("response: " + JSON.stringify(response));
+
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("Test forbidden access with valid token & valid role permissions", async () => {
+    await userModel.findOneAndUpdate({ email: user.email }, { role: "admin" });
+    const response = await request(app)
+      .get("/api/users")
+      .set("Authorization", "Bearer " + accessToken);
+    console.log("response: " + JSON.stringify(response));
+
+    expect(response.statusCode).toBe(200);
+  });
 });
