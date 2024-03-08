da637e3b5e63dde4e58164d5c91fc0c688c711e5
Shirel Pourostamian <shirelpo@gmail.com>
2024-03-08 19:40:35 +0200

    change dburl

--- src/utils/connect-to-DB.ts
+++ src/utils/connect-to-DB.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const dbUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`;
+// const dbUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`;
+const dbUrl = 'mongodb://server:123123123@127.0.0.1:21771/web_class'
 
 const connectDB = async () => {
   try {
